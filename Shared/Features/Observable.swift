5b7aaed995c36c33f17ecd68a0489c13e5581744
Charlie Fish <fishcharlie@me.com>
2019-05-30 11:49:05 -0600

    Adding basic Observable support

---
+++ Shared/Features/Observable.swift
@@ -0,0 +1,46 @@
+//
+//  Observable.swift
+//  Einstein
+//
+//  Created by Charlie Fish on 5/30/19.
+//
+
+import Foundation
+
+protocol ObservableClass {
+    associatedtype ValueType
+    associatedtype ListenerClosure
+
+    var value: ValueType { get set }
+}
+
+final class Observable<T>: ObservableClass {
+    typealias ValueType = T
+    typealias ListenerClosure = (_ new: T, _ old: T?) -> Void
+
+    private var observers: [ListenerClosure] = []
+
+    init(_ value: T) {
+        self.value = value
+    }
+
+    var value: T {
+        didSet {
+            notify(new: value, old: oldValue)
+        }
+    }
+
+    func addObserver(fire: Bool = false, _ closure: @escaping ListenerClosure) {
+        observers.append(closure)
+
+        if fire {
+            notify(new: value, old: nil)
+        }
+    }
+
+    private func notify(new: T, old: T?) {
+        observers.forEach { closure in
+            closure(new, old)
+        }
+    }
+}
